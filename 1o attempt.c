f8face76442e04fd2c3b16146b4dbeb5d0c25a29
stathismel <56596242+stathismel@users.noreply.github.com>
2022-11-28 01:23:06 +0200

    Update 1o attempt.c

--- 1o attempt.c
+++ 1o attempt.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
-#define MAXEXP 3
+#define MAXEXP 2
 
 int main(){
 
 
-    int n, k , r, p, z, temp, factor, previous=0;
-    int counter=0;                                                    /*plithos prwtwn paragontwn*/
-    int ps=0;                                                        /*teleio tetragwno - perfect square - an nai tote 1 alliws 0*/
-    int mertens=0;                                                    /*metritis gia M(n)*/
+    int b, j, n, k , r, p, z, temp, factor, previous=0, ZP=0, sum=0, afthonoi=0, elattomatikoi=0;
+    int counter=0;                                                    	/*plithos prwtwn paragontwn*/
+    int ps=0;                                                        	/*teleio tetragwno - perfect square - an nai tote 1 alliws 0*/
+    int mertens=0;                                                    	/*metritis gia M(n)*/
     int mobius=0;
     
     int ekthetis=MAXEXP;                    
-    for (p=1; ekthetis>0; ekthetis--){                                /*euresi tou 10 eis tin MAXEXP*/
+    for (p=1; ekthetis>0; ekthetis--){                                	/*euresi tou 10 eis tin MAXEXP*/
         p*=10;
     }
 
 
-    for(n=1;n<=(p+9);n++)                                            /*gia kathe n sto [1, (10^MAEXP)+9]*/
+    for(n=1;n<=(p+9);n++)                                            	/*gia kathe n sto [1, (10^MAEXP)+9]*/
     {    
         z=n;
         while(z==n){    
@@ -31,7 +31,7 @@ int main(){
                     }
                     previous=factor;
                     counter++;
-                    temp /= factor;                    /*temp=temp/factor*/
+                    temp /= factor;                                    	/*temp=temp/factor*/
                 }
                 if (factor==2){
                     factor=3;
@@ -68,7 +68,10 @@ int main(){
         }
             
         mertens+=mobius;
-
+        
+        if(mertens==0){
+            ZP++;
+        }
         counter=0;
         previous=0;
         ps=0;    
@@ -82,6 +85,33 @@ int main(){
                 printf("To M(%d) einai %d\n", n,mertens);
             }    
         }
-    }                
+    }
+    
+    printf("Found %d zero points of the Mertens function\n", ZP);
+    
+    int y=1000*ZP;
+    printf("Checking numbers in the range [2,%d]\n", y);
+    
+    for(b=2;b<=y;b++){
+        for(j=1;j<=(b/2);j++)
+        {
+            if (b%j==0){                                             		/*elegxos an to j einai diairetis tou n*/
+                sum=sum+j;    
+            }
+		}
+		if(sum==b){
+        	printf("Found perfect number: %d\n", b);
+        }
+        else if(sum<b){
+        	elattomatikoi++;
+        }
+        else if(sum>b){
+        	afthonoi++;
+        }
+        
+        sum=0;
+    } 
+    printf("Found %d deficient numbers\n", elattomatikoi);
+	printf("Found %d abundant numbers\n", afthonoi);              
     return 0;
-} 
+}  
